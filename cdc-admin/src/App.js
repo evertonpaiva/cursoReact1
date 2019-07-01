3758fb8c1e1a2f262a479c0cf992edcfdd70254a
Everton Paiva <evertonpaiva@gmail.com>
2019-07-01 10:37:25 -0300

    Refatorando o cadastro de autor para separar o form de cadastro e tabela de dados em componentes.

--- cdc-admin/src/App.js
+++ cdc-admin/src/App.js
@@ -1,65 +1,12 @@
 import React, { Component } from 'react';
-import InputCustomizado from './componentes/InputCustomizado';
-import BotaoSubmitCustomizado from './componentes/BotaoSubmitCustomizado';
+import {FormularioAutor, TabelaAutores }from './Autor';
+
 import './css/pure-min.css';
 import './css/side-menu.css';
-import $ from 'jquery';
+
+
 
 class App extends Component {
-
-  constructor() {
-    super();
-    this.state = {lista: []};
-    this.enviaForm = this.enviaForm.bind(this);
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
-  }
-
-  componentDidMount() {
-    $.ajax({
-        url:"http://localhost:8080/api/autores",
-        dataType: 'json',
-        success:function(resposta){
-          this.setState({lista:resposta});
-        }.bind(this)
-      }
-    );
-  }
-
-  enviaForm(evento){
-    evento.preventDefault();
-
-    $.ajax({
-      url:"http://localhost:8080/api/autores",
-      contentType: 'application/json',
-      dataType: 'json',
-      type: 'post',
-      data: JSON.stringify({
-        nome: this.state.nome,
-        email: this.state.email,
-        senha: this.state.senha
-      }),
-      success: function(resposta) {
-        this.setState({lista:resposta});
-      }.bind(this),
-      error: function(resposta) {
-        console.log("erro");
-      }
-    });
-  }
-
-  setNome(evento){
-    this.setState({nome:evento.target.value});
-  }
-
-  setEmail(evento){
-    this.setState({email:evento.target.value});
-  }
-
-  setSenha(evento){
-    this.setState({senha:evento.target.value});
-  }
 
   render() {
     return (
@@ -87,42 +34,9 @@ class App extends Component {
           </div>
 
           <div className="content" id="content">
-            <div className="pure-form pure-form-aligned">
 
-              <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
-
-
-                <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"/>
-                <InputCustomizado id="email" type="text" name="email" value={this.state.email} onChange={this.setEmail} label="E-mail"/>
-                <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"/>
-
-                <BotaoSubmitCustomizado label="Gravar"/>
-              </form>
-
-            </div>
-
-            <div>
-              <table className="pure-table">
-                <thead>
-                  <tr>
-                    <th>Nome</th>
-                    <th>email</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    this.state.lista.map((autor) => {
-                      return (
-                        <tr key={autor.id}>
-                          <td>{autor.nome}</td>
-                          <td>{autor.email}</td>
-                        </tr>
-                      );
-                    })
-                  }
-                </tbody>
-              </table>
-            </div>
+            <FormularioAutor/>
+            <TabelaAutores/>
 
           </div>
 
